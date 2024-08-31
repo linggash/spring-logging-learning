068fe606dc496f272f045b8d8461e3d6c83b4a2e
linggash <sempanalingga@gmail.com>
2024-08-31 09:53:09 +0700

    Init Project

---
+++ src/main/java/com/linggash/spring_logging_learning/SpringLoggingLearningApplication.java
@@ -0,0 +1,13 @@
+package com.linggash.spring_logging_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoggingLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLoggingLearningApplication.class, args);
+	}
+
+}
