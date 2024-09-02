05ebd64b6893d02f9797fa43298917ca4f0a5d48
linggash <sempanalingga@gmail.com>
2024-09-02 09:01:52 +0700

    Learned about Logging Level

--- src/test/java/com/linggash/spring_logging_learning/LoggingTest.java
+++ src/test/java/com/linggash/spring_logging_learning/LoggingTest.java
@@ -3,9 +3,11 @@ package com.linggash.spring_logging_learning;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @Slf4j
 @SpringBootTest
+@TestPropertySource("classpath:application-first.properties")
 public class LoggingTest {
 
     @Test
